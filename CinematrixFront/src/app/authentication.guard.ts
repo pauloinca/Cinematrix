7f98765bd2e67272d399e02e487b09c22419b2d5
pauloinca <paulohincar@gmail.com>
2021-12-29 15:08:44 -0300

    Update AuthenticationGuard

--- CinematrixFront/src/app/authentication.guard.ts
+++ CinematrixFront/src/app/authentication.guard.ts
@@ -2,19 +2,29 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private jwtHelper: JwtHelperService, private router: Router) {
+  private roles: string[] = [];
+
+  constructor(private router: Router, private auth: AuthService,
+    private tokenStorage: TokenStorageService) {
   }
 
-  canActivate() {
-    const token = localStorage.getItem("jwt");
-    if (token && !this.jwtHelper.isTokenExpired(token)) {
-      return true;
+  canActivate(next: ActivatedRouteSnapshot) {
+    const token = this.tokenStorage.getToken();
+
+    if (token) {
+      const user = this.tokenStorage.getUser();
+      this.roles = user.user.NivelAcesso;
+      const allowedRoles = next.data.allowedRoles;
+      if (allowedRoles.includes(this.roles))
+        return true;
     }
 
     this.router.navigate(["login"]);
